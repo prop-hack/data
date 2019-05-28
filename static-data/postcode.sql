1b614ad2d55823444c81ae5c0d32571ad22764a7
Iqbal Ahmed <iq.quit@gmail.com>
2019-05-28 11:58:00 +0100

    Add postcode outcode

--- static-data/postcode.sql
+++ static-data/postcode.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS `postcode`;
 CREATE TABLE `postcode` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `postcode` char(8) NOT NULL UNIQUE, -- with space postcode
+  `outcode` char(4) NOT NULL, -- first portion of postcode
   `lat_long` POINT NOT NULL SRID 4326, -- lat/long
   `country` enum('England','Wales','Scotland','Northern Ireland','Channel Islands','Isle of Man') NOT NULL,
   PRIMARY KEY (`id`),
